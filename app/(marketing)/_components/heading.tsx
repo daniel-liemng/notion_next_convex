c188be9f7da778a9dcfcf63f75939d4402af7d8c
Daniel <daniel.liemng@gmail.com>
2023-11-08 14:35:46 -0500

    authentication-clerk & database-convex

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,14 @@
 
 import { Button } from '@/components/ui/button';
 import { ArrowRight } from 'lucide-react';
+import { useConvexAuth } from 'convex/react';
+import Spinner from '@/components/spinner';
+import Link from 'next/link';
+import { SignInButton } from '@clerk/clerk-react';
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -11,13 +17,32 @@ const Heading = () => {
         <span className='underline'>Notion</span>
       </h1>
       <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
-        Notion us the connected workspace where
+        Notion is the connected workspace where
         <br />
         better, faster work happen.
       </h3>
-      <Button>
-        Enter Notion <ArrowRight className='h-4 w-4 ml-2' />
-      </Button>
+
+      {isLoading && (
+        <div className='w-full flex items-center justify-center'>
+          <Spinner size='lg' />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href='/documents'>
+            Enter Notion <ArrowRight className='h-4 w-4 ml-2' />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <>
+          <SignInButton mode='modal'>
+            <Button size='sm'>Get Notion free</Button>
+          </SignInButton>
+        </>
+      )}
     </div>
   );
 };
